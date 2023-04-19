d35a48e99a896ec837db530658b0edef6051cd20
William Tessari <wtessari@eneance.com>
2023-04-20 01:33:31 +0200

    card and background

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { s } from "./style";
+
+export const Home = () => {
+  return (
+    <s.Container>
+      <div>Bien le bonjour mes chers compatriotes (meme patriotique)</div>
+      <div>
+        Certains d'entre vous on du remarquer mon absence, d'autre pas du tout,
+        mais vous devez tous vous dire "Tiens c'est bien silencieux et bien
+        moins marrant depuis quelque temps" (meme demonstratif). Effectivement
+        je vous ai trahi mais cela n'est point de ma faute (meme explicatif)
+      </div>
+      <div>
+        Un peu plus serieusement (meme contextuel), J'aimerai remercier énéance
+        pour tout ce que cette entreprise m'a apporter (meme example) et pour
+        toutes les épreuves qui m'ont permis de grandir (meme exemple).
+      </div>
+      <div>
+        C'est avec tristesse que je quitte mon premier CDI (meme de vérité
+        absolue) qui m'a permis d'être en relation avec mes premiers clients
+        (meme exemple) et mes premiers collegues(meme exemple)
+      </div>
+      <div>J'aurai passé de très bon moment</div>
+      <div>
+        Maintenant voici une compilation de memes pour ceux dont mon absence va
+        être le plus dur à supporter (meme post-mortem)
+      </div>
+    </s.Container>
+  );
+};
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 10px;
+  width: 70%;
+  border-radius: 10px;
+
+  background-color: #fedaff;
+  color: #78017c;
+  box-shadow: 5px 5px 5px #cc8899;
+`;
+
+export const s = {
+  Container,
+};
